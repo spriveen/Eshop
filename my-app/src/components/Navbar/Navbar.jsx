c4e1cd0a933801b6d79de0b9f0447a44367735e0
spriveen <rpeanuka@gmail.com>
2025-03-06 08:30:09 -0800

    update DarkMode.jsx

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -9,6 +9,12 @@ const MenuLinks = [
   { id: 3, name: "About", link: "/about" },
   { id: 4, name: "Blogs", link: "/blog" }
 ];
+  const DropdownLinks =[
+    {id: 1, name: "Trending Product", link: "/#"},
+    {id: 2, name: "Best Selling", link: "/#"},
+    {id: 3, name: "Top Rated", link: "/#"},
+  ]
+
 
 const Navbar = () => {
   return (
@@ -34,9 +40,33 @@ const Navbar = () => {
                 ))}
                 {/* Drop down */}
                 <li className='relative cursor-pointer group'>
-               <a href='#' className=''>
+               <a href='#' className='flex items-center gap-
+               [2px] font-semibold text-gray-500
+               dark:hover:text-white py-2'>
                    Quick Link
                </a>
+
+               {/* Drop down links */}
+               <div className='absolute z-[9999] hidden 
+               group-hover:block w-[200px] rounded-md bg-white
+               shadow-md dark:bg-gray-900 p-2 text-black dark:text-white'>
+                <ul className='space-y-2'>
+                  {
+                    DropdownLinks.map((data, index)=>(
+                     <li>
+                      <a 
+                        className='text-gray-500
+                         dark:hover:text-white
+                        duration-200 inline-block w-full p-2 hover:bg-red-500/20
+                        rounded-md font-semibold'
+                      href={data.link}
+                      >
+                        {data.name}
+                        </a>
+                     </li> 
+                    ))}
+                </ul>
+               </div>
                <span>
                 <FaCaretDown className='group-hover:rotate-180 duration-300' />
                </span>
