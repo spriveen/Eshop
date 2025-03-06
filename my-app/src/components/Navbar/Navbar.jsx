b83470f627a4cd28b73adc9f68d752561a11b49c
spriveen <rpeanuka@gmail.com>
2025-03-06 01:35:22 -0800

    update Navbar.jsx

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+import React from 'react';
+import { IoMdSearch } from "react-icons/io";
+import {FaCartShopping} from "react-icons/fa6";
+
+const MenuLinks = [
+  { id: 1, name: "Home", link: "/#" },
+  { id: 2, name: "Shop", link: "/#shop" },
+  { id: 3, name: "About", link: "/about" },
+  { id: 4, name: "Blogs", link: "/blog" }
+];
 
 const Navbar = () => {
-  return <div>
-    <div>
-        
+  return (
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
+      <div className='py-4'>
+        <div className='container flex justify-between
+        items-center'>
+
+          {/* Logo and Menu Items */}
+          <div className='flex items-center gap-6'>
+            <a href='#' className='text-red-500 font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
+              Eshop
+            </a>
+            <div className='hidden lg:block'>
+              <ul className='flex items-center gap-6'>
+                {MenuLinks.map((data) => (
+                  <li key={data.id}>
+                    <a href={data.link} className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200'>
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          {/* Navbar Right Section */}
+          <div className='flex justify-between items-center
+          gap-4'>
+
+            {/* Search Bar */}
+            <div className='relative group hidden
+            sm:block'>
+              <input 
+                type='text' 
+                placeholder='Search'
+                className='
+                 search-bar
+                '/>
+              <IoMdSearch 
+              className='text-xl text-gray-600 group-hover:text-red-400
+               dark:text-gray-400 absolute top-1 -translate-y-1/2 right-3 duration-200 ' />
+            </div>
+            {/* Order button Section */}
+            <button className='realative p-3'>
+              <FaCartShopping className='text-xl text-gray-600
+              dark:text-gray-400 ' />
+              <div
+              className='w-4 h-4 bg-red-600 text-white
+              rounded-full absolute top-0 right-0 flex
+              items-center justify-center text-xs'>
+             4</div>
+            </button>
+            {/* Dark Mode Section (Placeholder) */}
+            <div></div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
-  </div>
-  
+  );
 }
 
-export default Navbar
+export default Navbar;
