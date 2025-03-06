ffc7269b36516895f5cfbc34185e964d1581b902
spriveen <rpeanuka@gmail.com>
2025-03-06 08:04:06 -0800

    update Dark mode.jsx

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IoMdSearch } from "react-icons/io";
-import {FaCartShopping} from "react-icons/fa6";
+import {FaCaretDown, FaCartShopping} from "react-icons/fa6";
+import DarkMode from './DarkMode';
 
 const MenuLinks = [
   { id: 1, name: "Home", link: "/#" },
@@ -16,11 +17,12 @@ const Navbar = () => {
         <div className='container flex justify-between
         items-center'>
 
-          {/* Logo and Menu Items */}
+          {/* Logo and links Items */}
           <div className='flex items-center gap-6'>
             <a href='#' className='text-red-500 font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
               Eshop
             </a>
+            {/* Menu Items */}
             <div className='hidden lg:block'>
               <ul className='flex items-center gap-6'>
                 {MenuLinks.map((data) => (
@@ -30,6 +32,15 @@ const Navbar = () => {
                     </a>
                   </li>
                 ))}
+                {/* Drop down */}
+                <li className='relative cursor-pointer group'>
+               <a href='#' className=''>
+                   Quick Link
+               </a>
+               <span>
+                <FaCaretDown className='group-hover:rotate-180 duration-300' />
+               </span>
+                </li>
               </ul>
             </div>
           </div>
@@ -39,34 +50,36 @@ const Navbar = () => {
           gap-4'>
 
             {/* Search Bar */}
-            <div className='relative group hidden
-            sm:block'>
-              <input 
-                type='text' 
-                placeholder='Search'
-                className='
-                 search-bar
-                '/>
-              <IoMdSearch 
-              className='text-xl text-gray-600 group-hover:text-red-400
-               dark:text-gray-400 absolute top-1 -translate-y-1/2 right-3 duration-200 ' />
-            </div>
-            {/* Order button Section */}
-            <button className='realative p-3'>
-              <FaCartShopping className='text-xl text-gray-600
-              dark:text-gray-400 ' />
-              <div
-              className='w-4 h-4 bg-red-600 text-white
-              rounded-full absolute top-0 right-0 flex
-              items-center justify-center text-xs'>
-             4</div>
-            </button>
-            {/* Dark Mode Section (Placeholder) */}
-            <div></div>
-
+            <div className="relative group hidden sm:block">
+            <input
+           type="text"
+           placeholder="Search"
+          className="w-full px-4 py-2 pr-10 border border-gray-300 rounded-md 
+          focus:outline-none focus:ring-2 focus:ring-red-400 dark:bg-gray-800
+           dark:border-gray-600 dark:text-white"
+            />
+           <IoMdSearch
+           className="text-xl text-gray-600 group-hover:text-red-400 dark:text-gray-400 
+            absolute top-1/2 right-3 transform -translate-y-1/2 duration-200"
+          />
           </div>
 
-        </div>
+            {/* Order button Section */}
+            <button className="relative p-1">
+           <FaCartShopping className="text-xl text-gray-600 dark:text-gray-400" />
+            <div
+        className="w-4 h-4 bg-red-600 text-white rounded-full absolute top-0 right-0 flex items-center 
+        justify-center text-xs translate-x-1/2 -translate-y-1/2"
+          >4
+      </div>
+       </button>
+
+            {/* Dark Mode Section (Placeholder) */}
+            <div>
+            <DarkMode/>
+            </div>
+             </div>
+          </div>
       </div>
     </div>
   );
